6deebd9365dc9b6c89f1954b624ddf213ea5f9b5
Maximilian Herkender <static@brokenfunction.com>
2012-03-24 10:57:35 -0700

    change generated lua parser from commonjs to js module

--- src/build_lua_parser.js
+++ src/build_lua_parser.js
@@ -2,6 +2,6 @@ var fs = require("fs");
 var Generator = require("../jison/lib/jison").Generator;
 var generator = new Generator(fs.readFileSync('src/lua.jison', 'utf8'), {type: "slr"})
 fs.writeFile("src/lua_parser.js", generator.generate({
-  moduleType: "commonjs",
+  moduleType: "js",
   moduleName: "lua_parser",
 }));
