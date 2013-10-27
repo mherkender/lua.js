c16087f5166417bb2bd666e3621feb868ff106a8
Florent POUJOL <florent.poujol@gmail.com>
2013-10-27 12:25:55 +0100

    Fixed wrong index in string.find()

--- src/lualib.js
+++ src/lualib.js
@@ -1064,7 +1064,7 @@ function luapattern_to_regex( pattern ) {
     
     "%u": "[A-Z\u00C0-\u00DF]", // À to ß
     "%U": "[^A-Z\u00C0-\u00DF]",
-    // below character 00FF upper case and lowercase characters are mixed
+    // below character 00FF, upper case and lowercase characters are mixed
     
     "%c": "[\u0000-\u001F]", // Control characters
     "%C": "[^\u0000-\u001F]",
@@ -1079,7 +1079,7 @@ function luapattern_to_regex( pattern ) {
     "%w": "\\w", // all word (alphanum) characters
     "%W": "\\W", 
 
-    // "%g": "", // all printable characters except space.  0021 (!) to  ?
+    // "%g": "", // all printable characters except space.  ! to  ?
   }
   
   for (var luaExp in replacements) {
@@ -1102,11 +1102,6 @@ function lua_gmatch_next(data) {
   data.s = data.s.substr(matchStartPos+match.length);
   return [match];
 }
-
-// SPRINTF
-// from  https://github.com/alexei/sprintf.js
-/*! sprintf.js | Copyright (c) 2007-2013 Alexandru Marasteanu <hello at alexei dot ro> | 3 clause BSD license */
-(function(e){function r(e){return Object.prototype.toString.call(e).slice(8,-1).toLowerCase()}function i(e,t){for(var n=[];t>0;n[--t]=e);return n.join("")}var t=function(){return t.cache.hasOwnProperty(arguments[0])||(t.cache[arguments[0]]=t.parse(arguments[0])),t.format.call(null,t.cache[arguments[0]],arguments)};t.format=function(e,n){var s=1,o=e.length,u="",a,f=[],l,c,h,p,d,v;for(l=0;l<o;l++){u=r(e[l]);if(u==="string")f.push(e[l]);else if(u==="array"){h=e[l];if(h[2]){a=n[s];for(c=0;c<h[2].length;c++){if(!a.hasOwnProperty(h[2][c]))throw t('[sprintf] property "%s" does not exist',h[2][c]);a=a[h[2][c]]}}else h[1]?a=n[h[1]]:a=n[s++];if(/[^s]/.test(h[8])&&r(a)!="number")throw t("[sprintf] expecting number but found %s",r(a));switch(h[8]){case"b":a=a.toString(2);break;case"c":a=String.fromCharCode(a);break;case"d":a=parseInt(a,10);break;case"e":a=h[7]?a.toExponential(h[7]):a.toExponential();break;case"f":a=h[7]?parseFloat(a).toFixed(h[7]):parseFloat(a);break;case"o":a=a.toString(8);break;case"s":a=(a=String(a))&&h[7]?a.substring(0,h[7]):a;break;case"u":a>>>=0;break;case"x":a=a.toString(16);break;case"X":a=a.toString(16).toUpperCase()}a=/[def]/.test(h[8])&&h[3]&&a>=0?"+"+a:a,d=h[4]?h[4]=="0"?"0":h[4].charAt(1):" ",v=h[6]-String(a).length,p=h[6]?i(d,v):"",f.push(h[5]?a+p:p+a)}}return f.join("")},t.cache={},t.parse=function(e){var t=e,n=[],r=[],i=0;while(t){if((n=/^[^\x25]+/.exec(t))!==null)r.push(n[0]);else if((n=/^\x25{2}/.exec(t))!==null)r.push("%");else{if((n=/^\x25(?:([1-9]\d*)\$|\(([^\)]+)\))?(\+)?(0|'[^$])?(-)?(\d+)?(?:\.(\d+))?([b-fosuxX])/.exec(t))===null)throw"[sprintf] huh?";if(n[2]){i|=1;var s=[],o=n[2],u=[];if((u=/^([a-z_][a-z_\d]*)/i.exec(o))===null)throw"[sprintf] huh?";s.push(u[1]);while((o=o.substring(u[0].length))!=="")if((u=/^\.([a-z_][a-z_\d]*)/i.exec(o))!==null)s.push(u[1]);else{if((u=/^\[(\d+)\]/.exec(o))===null)throw"[sprintf] huh?";s.push(u[1])}n[2]=s}else i|=2;if(i===3)throw"[sprintf] mixing positional and named placeholders is not (yet) supported";r.push(n)}t=t.substring(n[0].length)}return r};var n=function(e,n,r){return r=n.slice(0),r.splice(0,0,e),t.apply(null,r)};e.sprintf=t,e.vsprintf=n})(typeof exports!="undefined"?exports:window);
 
 // string
 lua_libs["string"] = {
@@ -1135,8 +1130,7 @@ lua_libs["string"] = {
       index = 1;
     else if (index < 0)
       index = s.length + index;
-
-    index = index-1;
+    index = index-1; // -1 because Lua's arrays index starts at 1 instead of 0
     s = s.substr(index);
     
     if (plain !== true) {
@@ -1150,7 +1144,7 @@ lua_libs["string"] = {
     
     start = s.indexOf(pattern);
     if (start != -1) {
-      start += index;
+      start += index+1; // +1 because Lua's arrays index starts at 1 instead of 0
       return [start, start+pattern.length-1];
     }
     else
